fcce08d75a43a4d4e227c72b2350dba182e69f12
fitipe <39530456+fitipe@users.noreply.github.com>
2023-01-14 18:45:21 -0300

    Update sketch.js

--- SITE/sketch.js
+++ SITE/sketch.js
@@ -14,6 +14,8 @@ let invrand;
 let invertido;
 let invnum;
 
+let lista = [];
+
 let sqr;
 
 function setup() {
@@ -112,6 +114,8 @@ function draw() {
         vidLoad
       );
     } else {
+			
+			let lista = ['./1.mp4', './3.mp4','./5.mp4', './6.mp4', './7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4']
       
       rand2 = Math.floor(Math.random()*lista.length);
       vid2num = lista[rand2];
