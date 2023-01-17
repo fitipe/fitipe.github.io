fd9ff17c866687d9af7b710d3f99097141dc9db2
fitipe <39530456+fitipe@users.noreply.github.com>
2023-01-16 23:45:44 -0300

    Update sketch.js

--- SITE/sketch.js
+++ SITE/sketch.js
@@ -1,22 +1,21 @@
 let rand1;
 let rand2;
 let rand3;
-let rand4;
-let rand5;
 
 let vid1num;
 let vid2num;
 let vid3num;
-let vid4num;
-let vid5num;
 
-let invrand;
-let invertido;
-let invnum;
+let invrand1;
+let invrand2;
+let invrand3;
 
 let lista = [];
 
 let sqr;
+
+let x = 0;
+let y = 0;
 
 function setup() {
 	sqr = Math.min(windowWidth, windowHeight);
@@ -26,46 +25,17 @@ function setup() {
 
 /// D R A W
 
-let x = 0;
-let y = 0;
-
 function draw() {
 
   background("white");
 
   if ( x == 0 ){
-
-    invrand = Math.floor(Math.random()*100);
-  
-    invnumlist = ['1st','2nd','3rd','4rth','5th'];
-
-    if (invrand < 15){
-      invertido = '1st';
-      invnum = invnumlist[0];
-    } else if (invrand < 30){
-      invertido = '2nd';
-      invnum = invnumlist[1];
-    } else if (invrand < 45){
-      invertido = '3rd';
-      invnum = invnumlist[2];
-    } else if (invrand < 60){  
-      invertido = '4rth';
-      invnum = invnumlist[3];
-    } else if (invrand < 75){  
-      invertido = '5th';
-      invnum = invnumlist[4];
-    } else if (invrand < 85){  
-      invertido = 'variable (super rare)';
-      invnum = '';
-    } else {
-      invertido = 'none (rare)';
-    }
-
-    if (invertido == 'variable (super rare)'){
-      invnum = random(invnumlist);
-    }
     
     if ( y == 0 ){
+
+      invrand1 = Math.floor(Math.random()*100);
+      invrand2 = Math.floor(Math.random()*100);
+      invrand3 = Math.floor(Math.random()*100);
 
       let lista = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4','./5.mp4','./6.mp4','./7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4','./15.mp4','./16.mp4','./17.mp4', './18.mp4','./19.mp4', './20.mp4', './21.mp4', './22.mp4', './23.mp4', './24.mp4', './25.mp4', './26.mp4', './27.mp4', './28.mp4','./29.mp4', './31.mp4', './32.mp4','./33.mp4','./34.mp4','./35.mp4', './36.mp4','./37.mp4', './38.mp4', './39.mp4', './40.mp4', './41.mp4', './42.mp4','./43.mp4']
   
@@ -81,14 +51,6 @@ function draw() {
       vid3num = lista[rand3];
       lista.splice(rand3,1)
       
-      rand4 = Math.floor(Math.random()*lista.length);
-      vid4num = lista[rand4];
-      lista.splice(rand4,1)
-      
-      rand5 = Math.floor(Math.random()*lista.length);
-      vid5num = lista[rand5];
-      lista.splice(rand5,1)
-      
       vid1 = createVideo(
         [vid1num],
         vidLoad1
@@ -103,21 +65,37 @@ function draw() {
         [vid3num],
         vidLoad3
       );
-    
-      vid4 = createVideo(
-        [vid4num],
-        vidLoad4
-      );
-    
-      vid5 = createVideo(
-        [vid5num],
-        vidLoad5
-      );
 
+    } 
+  }	
+
+  clear();
+	
+  x = x + 1;
+
+
+  if (x < 170){
+    if (invrand1 < 30){
+      invert();
     } else {
-			
+      desinvert();
+    }
+    video1();
+  }
+
+
+
+  if (x == 30){
+
+    if (y == 1){
+
+      invrand2 = Math.floor(Math.random()*100);
+      invrand3 = Math.floor(Math.random()*100);
+
       let lista = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4','./5.mp4','./6.mp4','./7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4','./15.mp4','./16.mp4','./17.mp4', './18.mp4','./19.mp4', './20.mp4', './21.mp4', './22.mp4', './23.mp4', './24.mp4', './25.mp4', './26.mp4', './27.mp4', './28.mp4','./29.mp4', './31.mp4', './32.mp4','./33.mp4','./34.mp4','./35.mp4', './36.mp4','./37.mp4', './38.mp4', './39.mp4', './40.mp4', './41.mp4', './42.mp4','./43.mp4']
-            
+      
+      lista.splice(rand1,1)
+      
       rand2 = Math.floor(Math.random()*lista.length);
       vid2num = lista[rand2];
       lista.splice(rand2,1)
@@ -125,14 +103,6 @@ function draw() {
       rand3 = Math.floor(Math.random()*lista.length);
       vid3num = lista[rand3];
       lista.splice(rand3,1)
-      
-      rand4 = Math.floor(Math.random()*lista.length);
-      vid4num = lista[rand4];
-      lista.splice(rand4,1)
-      
-      rand5 = Math.floor(Math.random()*lista.length);
-      vid5num = lista[rand5];
-      lista.splice(rand5,1)
   
       vid2 = createVideo(
         [vid2num],
@@ -143,38 +113,15 @@ function draw() {
         [vid3num],
         vidLoad3
       );
-    
-      vid4 = createVideo(
-        [vid4num],
-        vidLoad4
-      );
-    
-      vid5 = createVideo(
-        [vid5num],
-        vidLoad5
-      );
     }
-  }	
-
-  clear();
-	
-  x = x + 1;
-
-
-  if (x < 170){
-    if (invnum == '1st'){
-      invert();
-    } else {
-      desinvert();
-    }
-
-    video1();
   }
+
+
 
   if (x > 120 && x < 170){
     if (x % 13 == 0) {
       video2();
-      if (invnum == '2nd'){
+      if (invrand2 < 30){
         invert();
       } else {
         desinvert();
@@ -183,7 +130,7 @@ function draw() {
   
     if (x % 5 == 0) {
       video2();
-      if (invnum == '2nd'){
+      if (invrand2 < 30){
         invert();
       } else {
         desinvert();
@@ -192,7 +139,7 @@ function draw() {
   
     if (x % 2 == 0) {
       video2();
-      if (invnum == '2nd'){
+      if (invrand2 < 30){
         invert();
       } else {
         desinvert();
@@ -203,7 +150,7 @@ function draw() {
   if (x > 170 && x < 350){
     video2();
     if (x < 350){
-      if (invnum == '2nd'){
+      if (invrand2 < 30){
         invert();
       } else {
         desinvert();
@@ -214,7 +161,7 @@ function draw() {
   if (x > 300 && x < 350){
     if (x % 11 == 0) {
       video3();
-      if (invnum == '3rd'){
+      if (invrand3 < 30){
         invert();
       } else {
         desinvert();
@@ -223,7 +170,7 @@ function draw() {
   
     if (x % 5 == 0) {
       video3();
-      if (invnum == '3rd'){
+      if (invrand3 < 30){
         invert();
       } else {
         desinvert();
@@ -232,7 +179,7 @@ function draw() {
   
     if (x % 2 == 0) {
       video3();
-      if (invnum == '3rd'){
+      if (invrand3 < 30){
         invert();
       } else {
         desinvert();
@@ -240,18 +187,10 @@ function draw() {
     }
   }
   
-  if (x > 350 && x < 520){
-    video3();
-    if (invnum == '3rd'){
-      invert();
-    } else {
-      desinvert();
-    }
-  }
-	
-	
-	
-  if (x == 480){
+
+  if (x == 330){
+
+    invrand1 = Math.floor(Math.random()*100);
 
     let lista = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4','./5.mp4','./6.mp4','./7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4','./15.mp4','./16.mp4','./17.mp4', './18.mp4','./19.mp4', './20.mp4', './21.mp4', './22.mp4', './23.mp4', './24.mp4', './25.mp4', './26.mp4', './27.mp4', './28.mp4','./29.mp4', './31.mp4', './32.mp4','./33.mp4','./34.mp4','./35.mp4', './36.mp4','./37.mp4', './38.mp4', './39.mp4', './40.mp4', './41.mp4', './42.mp4','./43.mp4']
 
@@ -268,92 +207,23 @@ function draw() {
 	y += 1;
     }
   }
-  
+
+
+  if (x > 350 && x < 520){
+    video3();
+    if (invrand3 < 30){
+      invert();
+    } else {
+      desinvert();
+    }
+  }
 	
-  if (x > 470 && x < 520){
 
-    if (x % 11 == 0) {
-      video4();
-      if (invnum == '4rth'){
-        invert();
-      } else {
-        desinvert();
-      }
-    }
-    
-    if (x % 5 == 0) {
-      video4();
-      if (invnum == '4rth'){
-        invert();
-      } else {
-        desinvert();
-      }
-    }
-    
-    if (x % 2 == 0) {
-      video4();
-      if (invnum == '4rth'){
-        invert();
-      } else {
-        desinvert();
-      }
-    }
-  }
-    
-  if (x > 520 && x < 690){
-    video4();
-    if (invnum == '4rth'){
-      invert();
-    } else {
-      desinvert();
-    }
-  }
-
-  if (x > 640 && x < 690){
-
-    if (x % 11 == 0) {
-      video5();
-     if (invnum == '5th'){
-        invert();
-      } else {
-        desinvert();
-     }
-    }
-  
-    if (x % 5 == 0) {
-      video5();
-      if (invnum == '5th'){
-        invert();
-      } else {
-        desinvert();
-      }
-    }
-  
-    if (x % 2 == 0) {
-      video5();
-      if (invnum == '5th'){
-        invert();
-      } else {
-        desinvert();
-      }
-    }
-  }
-  
-  if (x > 690 && x < 890){
-    video5();
-    if (invnum == '5th'){
-      invert();
-    } else {
-      desinvert();
-    }
-  }
-
-
-  if (x > 840 && x < 890){
+  if (x > 475 && x < 520){
 
     if (x % 11 == 0) {
       video1();
-      if (invnum == '1st'){
+      if (invrand1 < 30){
         invert();
       } else {
         desinvert();
@@ -362,7 +232,7 @@ function draw() {
     
     if (x % 5 == 0) {
       video1();
-      if (invnum == '1st'){
+      if (invrand1 < 30){
         invert();
       } else {
         desinvert();
@@ -371,7 +241,7 @@ function draw() {
     
     if (x % 2 == 0) {
       video1();
-      if (invnum == '1st'){
+      if (invrand1 < 30){
         invert();
       } else {
         desinvert();
@@ -379,20 +249,19 @@ function draw() {
     }
   }
   
-  if (x > 890){
+  if (x > 520){
     video1();
-    if (invnum == '1st'){
+    if (invrand1 < 30){
       invert();
     } else {
       desinvert();
     }
   }
 
-  if (x >= 900){
+  if (x >= 530){
     x = 0;
   }
 }
-
 
 
 function vidLoad1() {
@@ -418,22 +287,6 @@ function vidLoad3() {
   vid3.center();
 }
 
-function vidLoad4() {
-  vid4.loop();
-  vid4.speed(1);
-  vid4.volume(0);
-  vid4.hide();
-  vid4.center();
-}
-
-function vidLoad5() {
-  vid5.loop();
-  vid5.speed(1);
-  vid5.volume(0);
-  vid5.hide();
-  vid5.center();
-}
-
 function invert (){
   drawingContext.filter = 'invert(100%)'
 }
@@ -456,12 +309,3 @@ function video2() {
 function video3() {
   image(vid3,0,0,sqr,sqr)
 }
-
-function video4() {
-  image(vid4,0,0,sqr,sqr)
-}
-
-function video5() {
-  image(vid5,0,0,sqr,sqr)
-}
-
