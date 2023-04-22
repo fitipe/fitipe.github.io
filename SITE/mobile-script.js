9e726f4a8951645d1d29d2507d5016eec5503fc7
fitipe <39530456+fitipe@users.noreply.github.com>
2023-04-22 19:50:36 -0300

    Create mobile-script.js

---
+++ SITE/mobile-script.js
@@ -0,0 +1,54 @@
+let media;
+let nextMedia;
+var sqr;
+var imageDuration = 5000; // time in milliseconds before selecting new image
+var timer; // holds the timer ID
+
+function setup() {
+  sqr = Math.min(windowWidth, windowHeight);
+  let canvas = createCanvas(sqr, sqr);
+  canvas.parent("canvas-container");
+  preloadMedia();
+}
+
+function preloadMedia() {
+  let images = ['./0.jpg', './1.jpg', './2.jpg', './3.jpg'];
+  let randomImage = images[Math.floor(Math.random() * images.length)];
+  media = createElement('img');
+  media.attribute('src', randomImage);
+  media.attribute('width', sqr);
+  media.attribute('height', sqr);
+  media.hide();
+  media.parent("canvas-container");
+
+  // set timer to select next image after a certain duration
+  timer = setInterval(selectNextImage, imageDuration);
+}
+
+function selectNextImage() {
+  let images = ['./0.jpg', './1.jpg', './2.jpg', './3.jpg'];
+  let nextImage = images[Math.floor(Math.random() * images.length)];
+  nextMedia = createElement('img');
+  nextMedia.attribute('src', nextImage);
+  nextMedia.attribute('width', sqr);
+  nextMedia.attribute('height', sqr);
+  nextMedia.hide();
+  nextMedia.parent("canvas-container");
+
+  // remove current media after next media loads
+  nextMedia.elt.onload = function() {
+    media.remove();
+    media = nextMedia;
+    nextMedia = undefined;
+  };
+}
+
+function draw() {
+  if (media !== undefined) {
+    // clear the canvas
+    clear();
+
+    // draw the current media
+    image(media, 0, 0, sqr, sqr);
+  }
+}
