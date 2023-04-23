ae55fe624cbc084d0db190e971983e7b6169789c
fitipe <39530456+fitipe@users.noreply.github.com>
2023-04-23 14:09:51 -0300

    Update desktop-script.js

--- RANDOMVAR/desktop-script.js
+++ RANDOMVAR/desktop-script.js
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function preloadMedia() {
-    let videos = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4'];
+    let videos = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4','./5.mp4','./6.mp4','./7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4','./15.mp4','./16.mp4','./17.mp4', './18.mp4','./19.mp4', './20.mp4', './21.mp4', './22.mp4', './23.mp4', './24.mp4', './25.mp4', './26.mp4', './27.mp4', './28.mp4','./29.mp4','./30.mp4', './31.mp4', './32.mp4','./33.mp4','./34.mp4','./35.mp4', './36.mp4','./37.mp4'];
     let randomVideo = videos[Math.floor(Math.random() * videos.length)];
     media = createVideo(randomVideo);
     media.elt.muted = true;
@@ -25,7 +25,7 @@ function preloadMedia() {
 }
 
 function selectNextVideo() {
-  let videos = ['./0.mp4', './1.mp4', './2.mp4','./3.mp4','./4.mp4'];
+  let videos = ['./0.mp4','./1.mp4','./2.mp4','./3.mp4','./4.mp4','./5.mp4','./6.mp4','./7.mp4', './8.mp4', './9.mp4', './10.mp4', './11.mp4', './12.mp4', './13.mp4', './14.mp4','./15.mp4','./16.mp4','./17.mp4', './18.mp4','./19.mp4', './20.mp4', './21.mp4', './22.mp4', './23.mp4', './24.mp4', './25.mp4', './26.mp4', './27.mp4', './28.mp4','./29.mp4','./30.mp4', './31.mp4', './32.mp4','./33.mp4','./34.mp4','./35.mp4', './36.mp4','./37.mp4'];
   let nextVideo = videos[Math.floor(Math.random() * videos.length)];
   nextMedia = createVideo(nextVideo);
   nextMedia.elt.muted = true;
